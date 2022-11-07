9adc6250195219829b0816be960e29ccad1ce4ef
Pohorielova <anna.pogorielova@gmail.com>
2022-11-07 14:29:27 +0100

    Update task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const STORAGE_KEY = "videoplayer-current-time";
+
+
+const onPlay = function(data) {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(data.seconds))
+      // data is an object containing properties specific to that event
+  };
+player.on('timeupdate',throttle(onPlay, 1000));
+
+const saveTime = localStorage.getItem(STORAGE_KEY);
+if (saveTime)
+  player.setCurrentTime(saveTime).then(function (seconds) {
+
+  });
 
